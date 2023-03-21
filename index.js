bd8ed558a18aef8708f855e56fcb57776803763d
Abdul Fattah Ikhsan <skull.saders18@gmail.com>
2023-03-21 16:15:10 +0700

    feat: supports JWE

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var Webtask = require('webtask-tools');
 var bodyParser = require('body-parser');
-const { SignJWT } = require('jose/dist/node/cjs/jwt/sign');
-const { parseJwk } = require('jose/dist/node/cjs/jwk/parse');
-const { jwtVerify } = require('jose/dist/node/cjs/jwt/verify');
+const { SignJWT, jwtVerify, importJWK: parseJwk, compactDecrypt } = require('jose');
 const crypto = require("crypto");
 const uuid = require("uuid");
 const axios = require("axios").default;
@@ -29,23 +27,20 @@ app.get('/authorize', (req, res) => {
         return res.send(401, 'invalid client_id');
     }
     var url = `https://${context.data.AUTH0_CUSTOM_DOMAIN}${req.url}&ndi_state=${req.query.state}&ndi_nonce=${req.query.code_challenge}&singpass=true`;
-    console.log("authorize - authorization url", url);
+    console.log("/authorize - authorization url", url);
     res.redirect(url);
 });
 
 app.post('/token', async function (req, res) {
     const context = req.webtaskContext;
     const { client_id, client_secret, code, code_verifier, redirect_uri } = req.body;
-    console.log("token - client_id", client_id);
-    console.log("token - client_secret", client_secret);
-    console.log("token - code", code);
-    console.log("token - code_verifier", code_verifier);
-    console.log("token - redirect_uri", redirect_uri);
+    console.log('/token - body', req.body);
     if (!client_id || !client_secret) {
         return res.send(400, 'missing client_id / client_secret');
     }
     if (context.data.AUTH0_CLIENT_ID === client_id && context.data.AUTH0_CLIENT_SECRET === client_secret) {
         const client_assertion = await generatePrivateKeyJWT(context.data);
+        console.log('/token - client_assertion', client_assertion);
         var options = {
             method: 'POST',
             url: `${context.data.SINGPASS_ENVIRONMENT}/token`,
@@ -61,20 +56,44 @@ app.post('/token', async function (req, res) {
         };
         try {
             const response = await axios.request(options);
+            console.log('/token - response.data - 1', response.data);
             const { id_token } = response.data;
             const publicKey = await loadPublicKey(context.data);
+            console.log('/token - publicKey', publicKey);
             const code_v = new TextEncoder().encode(code_verifier);
+            console.log('/token - code_v', code_v);
             const code_v_s256 = crypto.createHash('sha256').update(code_v).digest('base64').replace(/\//g, '_').replace(/\+/g, '-').replace(/=/g, '');
             console.log(`nonce expected: ${code_v_s256}`);
-            const { payload, protectedHeader } = await jwtVerify(id_token, publicKey, {
-                issuer: context.data.ISSUER,
-                audience: context.data.CLIENT_ID,
-            });
-            if (payload.nonce !== code_v_s256) {
-                return res.send(400, 'nonce mismatch');
+            if (id_token.split('.').length === 5) {
+                console.log('/token - id_token.length === 5', true);
+                const privateKey = await loadPrivateKeyEnc(context.data);
+                let { plaintext, protectedHeader: protectedHeaderEnc } = await compactDecrypt(id_token, privateKey);
+                let id_token_decrypt = new TextDecoder().decode(plaintext);
+                console.log('/token - id_token_decrypt', id_token_decrypt);
+                const { payload, protectedHeader } = await jwtVerify(id_token_decrypt, publicKey, {
+                    issuer: context.data.SINGPASS_ENVIRONMENT,
+                    audience: context.data.SINGPASS_CLIENT_ID,
+                });
+                if (payload.nonce !== code_v_s256) {
+                    return res.send(400, 'nonce mismatch');
+                } else {
+                    response.data.payload = payload;
+                    response.data.id_token = id_token_decrypt;
+                    console.log('/token - response.data - 2', response.data);
+                    return res.status(200).send(response.data);
+                }
             } else {
-                response.data.payload = payload;
-                return res.status(200).send(response.data);
+                const { payload, protectedHeader } = await jwtVerify(id_token, publicKey, {
+                    issuer: context.data.SINGPASS_ENVIRONMENT,
+                    audience: context.data.SINGPASS_CLIENT_ID,
+                });
+                if (payload.nonce !== code_v_s256) {
+                    return res.send(400, 'nonce mismatch');
+                } else {
+                    response.data.payload = payload;
+                    console.log('/token - response.data - 2', response.data);
+                    return res.status(200).send(response.data);
+                }
             }
 
         } catch (error) {
@@ -94,15 +113,26 @@ app.post('/token', async function (req, res) {
 app.post('/verify', async function (req, res) {
     try {
         const { id_token } = response.body;
-        console.log("verify - id_token", id_token);
+        console.log("/verify - id_token", id_token);
         if (!id_token) {
             return res.status(400).send('ID_TOKEN required');
         }
         const publicKey = await loadPublicKey(context.data);
-        console.log("verify - publicKey", publicKey);
+        console.log("/verify - publicKey", publicKey);
+        if (id_token.split('.').length === 5) {
+            console.log("/verify - id_token.length === 5", true);
+            const privateKey = await loadPrivateKeyEnc(context.data);
+            let { plaintext, protectedHeader: protectedHeaderEnc } = await compactDecrypt(id_token, privateKey);
+            let id_token_decrypt = new TextDecoder().decode(plaintext);
+            const { payload, protectedHeader } = await jwtVerify(id_token_decrypt, publicKey, {
+                issuer: context.data.SINGPASS_ENVIRONMENT,
+                audience: context.data.SINGPASS_CLIENT_ID,
+            })
+            return res.status(200).send(payload);
+        }
         const { payload, protectedHeader } = await jwtVerify(id_token, publicKey, {
-            issuer: context.data.ISSUER,
-            audience: context.data.CLIENT_ID
+            issuer: context.data.SINGPASS_ENVIRONMENT,
+            audience: context.data.SINGPASS_CLIENT_ID,
         })
         return res.status(200).send(payload);
     } catch (error) {
@@ -121,6 +151,17 @@ async function loadPrivateKey(config) {
         const { keys } = response.data;
         keys[0].d = config.RELYING_PARTY_PRIVATE_KEY;
         return await parseJwk(keys[0], config.SINGPASS_SIGNING_ALG);;
+    } catch (e) {
+        return e;
+    }
+};
+
+async function loadPrivateKeyEnc(config) {
+    try {
+        const response = await axios.get(config.RELYING_PARTY_JWKS_ENDPOINT);
+        const { keys } = response.data;
+        keys[1].d = config.RELYING_PARTY_PRIVATE_KEY_ENC;
+        return await parseJwk(keys[1], config.SINGPASS_SIGNING_ALG);;
     } catch (e) {
         return e;
     }
